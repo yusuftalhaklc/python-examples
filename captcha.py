b2c97a7bc8c0e99820101da32c6a9920bd3d7b5f
Yusuf Talha KILIÇ <46709263+codception@users.noreply.github.com>
2021-11-26 21:08:48 +0300

    Update captcha.py

--- captcha.py
+++ captcha.py
@@ -1,13 +1,19 @@
 # python // YUSUF KILIC
 import random
-list = "ABCDEFGHIJKLMOPRSTUVYZQWXabcdefghijklmnoprstuvyzqwx0123456789"
-mix = random.sample(list,6)
-id = ','.join(map(str, mix))
-captcha =id.replace(',','')
-print("Your Security Code : ",captcha)
+
+listOfCharacters = "ABCDEFGHIJKLMOPRSTUVYZQWXabcdefghijklmnoprstuvyzqwx0123456789"
+mixCharacters = random.sample(listOfCharacters,6)
+
+captchaCode = ','.join(map(str, mixCharacters))
+captchaCode = id.replace(',','')
+
+print("Your Security Code : ", captchaCode)
+
 while True:
- user_captcha = input("Verify : ")
- if captcha == user_captcha:
+ getCaptcha = input("Verify : ")
+ 
+ if (captchaCode == getCaptcha):
   print("Succesfuly Entered")
   break
+  
  print("Please try again")
