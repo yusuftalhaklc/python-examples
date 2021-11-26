5d99cc62594191065e213b192395fdd7da0dc4c0
Yusuf Talha KILIÇ <46709263+codception@users.noreply.github.com>
2021-11-26 21:13:28 +0300

    Update internettenveriçekme.py

--- internettenveriçekme.py
+++ internettenveriçekme.py
@@ -1,15 +1,19 @@
 # python
 import requests
 from bs4 import BeautifulSoup
-imdb_url = "https://www.imdb.com/chart/top"
-r = requests.get(imdb_url)
-soup = BeautifulSoup(r.content,"html.parser")
-gelen_veri = soup.find_all("table",{"class":"chart full-width"})
-filmtablo = (gelen_veri[0].contents) [len(gelen_veri[0].contents)-2]
-filmtablo = filmtablo.find_all("tr")
-for film in filmtablo:
-    filmbasliklari = film.find_all("td",{"class":"titleColumn"})
-    filmismi = filmbasliklari[0].text
-    filmismi = filmismi.replace("\n","")
-    print(filmismi)
+
+urlImdbWebSite = "https://www.imdb.com/chart/top"
+tagImdb = requests.get(urlImdbWebSite)
+tagImdb = BeautifulSoup(tagImdb.content,"html.parser")
+
+dataImdb = tagImdb.find_all("table",{"class":"chart full-width"})
+
+tableOfFilms = (dataImdb[0].contents) [len(dataImdb[0].contents)-2]
+tableOfFilms = tableOfFilms.find_all("tr")
+
+for film in tableOfFilms:
+    headersFılms = film.find_all("td",{"class":"titleColumn"})
+    nameOfFilm = headersFılms[0].text
+    nameOfFilm = nameOfFilm.replace("\n","")
+    print(nameOfFilm)
     print("--------------------------------------------------------")
